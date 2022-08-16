a785df0fc60dff8e349ea6cff86f939fa1424bba
nervinjf <nervinjflores@gmail.com>
2022-08-15 21:36:42 -0400

    Actualizacion de style, js y html

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ background: rgba(${colors}, ${opaci});
 <br>
 backdrop-Filter: ${opaci};
 <br>
-backdrop-Filter: Blur(${blurr});
+backdrop-Filter: ${blurr};
 <br>
 width: ${sizes};
 <br>
@@ -59,15 +59,15 @@ blur.addEventListener('mouseup', (e) => {
 });
 
 blur.addEventListener("input", (e) =>{
-  const blurr = `Blur(${e.target.value}px)`;
-  square.style.backdropFilter = `Blur(${e.target.value}px)`;
+  blurr = `Blur(${e.target.value}px)`;
+  square.style.backdropFilter = blurr;
   e.target.nextElementSibling.textContent = blurr;
   text.innerHTML = `<p class="textContent">
 background: rgba(${colors}, ${opaci});
 <br>
 backdrop-Filter: ${opaci};
 <br>
-backdrop-Filter: Blur(${blurr});
+backdrop-Filter: ${blurr};
 <br>
 width: ${sizes};
 <br>
@@ -87,7 +87,7 @@ background: rgba(${colors}, ${opaci});
 <br>
 backdrop-Filter: ${opaci};
 <br>
-backdrop-Filter: Blur(${blurr});
+backdrop-Filter: ${blurr};
 <br>
 width: ${sizes};
 <br>
@@ -118,7 +118,7 @@ background: rgba(${colors}, ${opaci});
 <br>
 backdrop-Filter: ${opaci};
 <br>
-backdrop-Filter: Blur(${blurr});
+backdrop-Filter: ${blurr};
 <br>
 width: ${sizes};
 <br>
@@ -146,7 +146,7 @@ background: rgba(${colors}, ${opaci});
 <br>
 backdrop-Filter: ${opaci};
 <br>
-backdrop-Filter: Blur(${blurr});
+backdrop-Filter: ${blurr};
 <br>
 width: ${sizes};
 <br>
